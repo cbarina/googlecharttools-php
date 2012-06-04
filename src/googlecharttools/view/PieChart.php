7e6c57c09fbb072a2c7d1c3a68685786b64dcdf9
patrick strobel swt <patrick.strobel.swt@gmail.com>
2012-06-04 18:01:03 +0000

    Added pie-chart class

---
+++ src/googlecharttools/view/PieChart.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Copyright 2012 Patrick Strobel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Patrick Strobel
+ * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
+ * @link http://code.google.com/p/googlecharttools-php
+ * @version $Id$
+ * @package default
+ */
+
+namespace googlecharttools\view;
+
+use googlecharttools\view\Chart;
+
+/**
+ * Creates a pie chart.
+ *
+ * <b>Data format:</b><br />
+ * The line chart requires a {@link DataTable} with has to columns.
+ * The first column is used for the labels and and the second one for the values.
+ *
+ * See {@link https://google-developers.appspot.com/chart/interactive/docs/gallery/piechart}
+ * for examples and detailed background information on the required data format.
+ *
+ * @package view
+ */
+class PieChart extends Chart {
+
+}
+?>
