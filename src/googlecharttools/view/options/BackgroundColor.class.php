23fb67108999c34598d574f3674ad473eed88dd0
patrick strobel swt <patrick.strobel.swt@gmail.com>
2012-10-01 14:34:31 +0000

    Replaced ID-Keyword by revision

--- src/googlecharttools/view/options/BackgroundColor.class.php
+++ src/googlecharttools/view/options/BackgroundColor.class.php
@@ -20,7 +20,7 @@
  * @author Patrick Strobel
  * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
  * @link http://code.google.com/p/googlecharttools-php
- * @version $Id$
+ * @version $Revision$
  * @package view
  * @subpackage options
  */
--- src/index.php
+++ src/index.php
@@ -24,7 +24,7 @@
  * @author Patrick Strobel
  * @license http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
  * @link http://code.google.com/p/googlecharttools-php
- * @version $Id$
+ * @version $Revision$
  * @package default
  */
 use googlecharttools\model\Cell;
